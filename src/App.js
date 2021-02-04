fd499a110d1a1d13de2e780ea491d1d7108cb3fc
Corey <aingc@users.noreply.github.com>
2021-02-03 16:42:29 -0800

    Bare bones weather app, but have to search specific city

--- src/App.js
+++ src/App.js
@@ -55,7 +55,15 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != 'undefined'
+          ? weather.main.temp > 16
+            ? 'app warm'
+            : 'app'
+          : 'app'
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -67,14 +75,24 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15C</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != 'undefined' ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp * 10) / 10}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </main>
     </div>
   );
